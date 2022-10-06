b1e4d8912d0e18922f45d48ab9fe2d82a3e6e514
mohammed alsersawi <107371667+mohammedalsersawi@users.noreply.github.com>
2022-10-07 00:53:15 +0300

    hamo v5.1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('email')->unique();
             $table->string('password');
+            $table->enum('type', ['Admin', 'Coach' , 'Trainee']);
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
