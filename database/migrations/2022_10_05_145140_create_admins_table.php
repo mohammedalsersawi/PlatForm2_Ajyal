7a728629b63f4e843c2a491ff822cdd88dd9dd6e
mohammed alsersawi <107371667+mohammedalsersawi@users.noreply.github.com>
2022-10-23 17:49:26 +0300

    v 7.6

--- database/migrations/2022_10_05_145140_create_admins_table.php
+++ database/migrations/2022_10_05_145140_create_admins_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('admins', function (Blueprint $table) {
+            $table->integer('id');
             $table->foreignId('user_id')->constrained('users');
             $table->string('name');
             $table->integer('national_id')->unique();
